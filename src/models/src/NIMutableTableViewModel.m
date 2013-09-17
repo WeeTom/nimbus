9f3b7e1c9e61d6d2ea43a1d277f81d62f29a6353
WeeTom <lovinghmy@gmail.com>
2013-09-17 13:14:05 +0800

    upodate

--- src/models/src/NIMutableTableViewModel.m
+++ src/models/src/NIMutableTableViewModel.m
@@ -36,85 +36,85 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NSArray *)addObject:(id)object {
-  NITableViewModelSection* section = self.sections.count == 0 ? [self _appendSection] : self.sections.lastObject;
-  [section.mutableRows addObject:object];
-  return [NSArray arrayWithObject:[NSIndexPath indexPathForRow:section.mutableRows.count - 1
-                                                     inSection:self.sections.count - 1]];
+    NITableViewModelSection* section = self.sections.count == 0 ? [self _appendSection] : self.sections.lastObject;
+    [section.mutableRows addObject:object];
+    return [NSArray arrayWithObject:[NSIndexPath indexPathForRow:section.mutableRows.count - 1
+                                                       inSection:self.sections.count - 1]];
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NSArray *)addObject:(id)object toSection:(NSUInteger)sectionIndex {
-  NIDASSERT(sectionIndex >= 0 && sectionIndex < self.sections.count);
-  NITableViewModelSection *section = [self.sections objectAtIndex:sectionIndex];
-  [section.mutableRows addObject:object];
-  return [NSArray arrayWithObject:[NSIndexPath indexPathForRow:section.mutableRows.count - 1
-                                                     inSection:sectionIndex]];
+    NIDASSERT(sectionIndex >= 0 && sectionIndex < self.sections.count);
+    NITableViewModelSection *section = [self.sections objectAtIndex:sectionIndex];
+    [section.mutableRows addObject:object];
+    return [NSArray arrayWithObject:[NSIndexPath indexPathForRow:section.mutableRows.count - 1
+                                                       inSection:sectionIndex]];
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NSArray *)addObjectsFromArray:(NSArray *)array {
-  NSMutableArray* indices = [NSMutableArray array];
-  for (id object in array) {
-    [indices addObject:[[self addObject:object] objectAtIndex:0]];
-  }
-  return indices;
+    NSMutableArray* indices = [NSMutableArray array];
+    for (id object in array) {
+        [indices addObject:[[self addObject:object] objectAtIndex:0]];
+    }
+    return indices;
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NSArray *)insertObject:(id)object atRow:(NSUInteger)row inSection:(NSUInteger)sectionIndex {
-  NIDASSERT(sectionIndex >= 0 && sectionIndex < self.sections.count);
-  NITableViewModelSection *section = [self.sections objectAtIndex:sectionIndex];
-  [section.mutableRows insertObject:object atIndex:row];
-  return [NSArray arrayWithObject:[NSIndexPath indexPathForRow:row inSection:sectionIndex]];
+    NIDASSERT(sectionIndex >= 0 && sectionIndex < self.sections.count);
+    NITableViewModelSection *section = [self.sections objectAtIndex:sectionIndex];
+    [section.mutableRows insertObject:object atIndex:row];
+    return [NSArray arrayWithObject:[NSIndexPath indexPathForRow:row inSection:sectionIndex]];
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NSArray *)removeObjectAtIndexPath:(NSIndexPath *)indexPath {
-  NIDASSERT(indexPath.section < (NSInteger)self.sections.count);
-  if (indexPath.section >= (NSInteger)self.sections.count) {
-    return nil;
-  }
-  NITableViewModelSection* section = [self.sections objectAtIndex:indexPath.section];
-  NIDASSERT(indexPath.row < (NSInteger)section.mutableRows.count);
-  if (indexPath.row >= (NSInteger)section.mutableRows.count) {
-    return nil;
-  }
-  [section.mutableRows removeObjectAtIndex:indexPath.row];
-  return [NSArray arrayWithObject:indexPath];
+    NIDASSERT(indexPath.section < (NSInteger)self.sections.count);
+    if (indexPath.section >= (NSInteger)self.sections.count) {
+        return nil;
+    }
+    NITableViewModelSection* section = [self.sections objectAtIndex:indexPath.section];
+    NIDASSERT(indexPath.row < (NSInteger)section.mutableRows.count);
+    if (indexPath.row >= (NSInteger)section.mutableRows.count) {
+        return nil;
+    }
+    [section.mutableRows removeObjectAtIndex:indexPath.row];
+    return [NSArray arrayWithObject:indexPath];
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NSIndexSet *)addSectionWithTitle:(NSString *)title {
-  NITableViewModelSection* section = [self _appendSection];
-  section.headerTitle = title;
-  return [NSIndexSet indexSetWithIndex:self.sections.count - 1];
+    NITableViewModelSection* section = [self _appendSection];
+    section.headerTitle = title;
+    return [NSIndexSet indexSetWithIndex:self.sections.count - 1];
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NSIndexSet *)insertSectionWithTitle:(NSString *)title atIndex:(NSUInteger)index {
-  NITableViewModelSection* section = [self _insertSectionAtIndex:index];
-  section.headerTitle = title;
-  return [NSIndexSet indexSetWithIndex:index];
+    NITableViewModelSection* section = [self _insertSectionAtIndex:index];
+    section.headerTitle = title;
+    return [NSIndexSet indexSetWithIndex:index];
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NSIndexSet *)removeSectionAtIndex:(NSUInteger)index {
-  NIDASSERT(index >= 0 && index < self.sections.count);
-  [self.sections removeObjectAtIndex:index];
-  return [NSIndexSet indexSetWithIndex:index];
+    NIDASSERT(index >= 0 && index < self.sections.count);
+    [self.sections removeObjectAtIndex:index];
+    return [NSIndexSet indexSetWithIndex:index];
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)updateSectionIndex {
-  [self _compileSectionIndex];
+    [self _compileSectionIndex];
 }
 
 
@@ -125,28 +125,28 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NITableViewModelSection *)_appendSection {
-  if (nil == self.sections) {
-    self.sections = [NSMutableArray array];
-  }
-  NITableViewModelSection* section = nil;
-  section = [[NITableViewModelSection alloc] init];
-  section.rows = [NSMutableArray array];
-  [self.sections addObject:section];
-  return section;
+    if (nil == self.sections) {
+        self.sections = [NSMutableArray array];
+    }
+    NITableViewModelSection* section = nil;
+    section = [[NITableViewModelSection alloc] init];
+    section.rows = [NSMutableArray array];
+    [self.sections addObject:section];
+    return section;
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NITableViewModelSection *)_insertSectionAtIndex:(NSUInteger)index {
-  if (nil == self.sections) {
-    self.sections = [NSMutableArray array];
-  }
-  NITableViewModelSection* section = nil;
-  section = [[NITableViewModelSection alloc] init];
-  section.rows = [NSMutableArray array];
-  NIDASSERT(index >= 0 && index <= self.sections.count);
-  [self.sections insertObject:section atIndex:index];
-  return section;
+    if (nil == self.sections) {
+        self.sections = [NSMutableArray array];
+    }
+    NITableViewModelSection* section = nil;
+    section = [[NITableViewModelSection alloc] init];
+    section.rows = [NSMutableArray array];
+    NIDASSERT(index >= 0 && index <= self.sections.count);
+    [self.sections insertObject:section atIndex:index];
+    return section;
 }
 
 
@@ -157,57 +157,57 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-  if ([self.delegate respondsToSelector:@selector(tableViewModel:canEditObject:atIndexPath:inTableView:)]) {
-    id object = [self objectAtIndexPath:indexPath];
-    return [self.delegate tableViewModel:self canEditObject:object atIndexPath:indexPath inTableView:tableView];
-  } else {
-    return NO;
-  }
+    if ([self.delegate respondsToSelector:@selector(tableViewModel:canEditObject:atIndexPath:inTableView:)]) {
+        id object = [self objectAtIndexPath:indexPath];
+        return [self.delegate tableViewModel:self canEditObject:object atIndexPath:indexPath inTableView:tableView];
+    } else {
+        return NO;
+    }
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-  id object = [self objectAtIndexPath:indexPath];
-  if (editingStyle == UITableViewCellEditingStyleDelete) {
-    BOOL shouldDelete = YES;
-    if ([self.delegate respondsToSelector:@selector(tableViewModel:shouldDeleteObject:atIndexPath:inTableView:)]) {
-      shouldDelete = [self.delegate tableViewModel:self shouldDeleteObject:object atIndexPath:indexPath inTableView:tableView];
+    id object = [self objectAtIndexPath:indexPath];
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        BOOL shouldDelete = YES;
+        if ([self.delegate respondsToSelector:@selector(tableViewModel:shouldDeleteObject:atIndexPath:inTableView:)]) {
+            shouldDelete = [self.delegate tableViewModel:self shouldDeleteObject:object atIndexPath:indexPath inTableView:tableView];
+        }
+        if (shouldDelete) {
+            NSArray *indexPaths = [self removeObjectAtIndexPath:indexPath];
+            UITableViewRowAnimation animation = UITableViewRowAnimationAutomatic;
+            if ([self.delegate respondsToSelector:@selector(tableViewModel:deleteRowAnimationForObject:atIndexPath:inTableView:)]) {
+                animation = [self.delegate tableViewModel:self deleteRowAnimationForObject:object atIndexPath:indexPath inTableView:tableView];
+            }
+            [tableView deleteRowsAtIndexPaths:indexPaths withRowAnimation:animation];
+        }
     }
-    if (shouldDelete) {
-      NSArray *indexPaths = [self removeObjectAtIndexPath:indexPath];
-      UITableViewRowAnimation animation = UITableViewRowAnimationAutomatic;
-      if ([self.delegate respondsToSelector:@selector(tableViewModel:deleteRowAnimationForObject:atIndexPath:inTableView:)]) {
-        animation = [self.delegate tableViewModel:self deleteRowAnimationForObject:object atIndexPath:indexPath inTableView:tableView];
-      }
-      [tableView deleteRowsAtIndexPaths:indexPaths withRowAnimation:animation];
-    }
-  }
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-  if ([self.delegate respondsToSelector:@selector(tableViewModel:canMoveObject:atIndexPath:inTableView:)]) {
-    id object = [self objectAtIndexPath:indexPath];
-    return [self.delegate tableViewModel:self canMoveObject:object atIndexPath:indexPath inTableView:tableView];
-  } else {
-    return NO;
-  }
+    if ([self.delegate respondsToSelector:@selector(tableViewModel:canMoveObject:atIndexPath:inTableView:)]) {
+        id object = [self objectAtIndexPath:indexPath];
+        return [self.delegate tableViewModel:self canMoveObject:object atIndexPath:indexPath inTableView:tableView];
+    } else {
+        return NO;
+    }
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
-  id object = [self objectAtIndexPath:sourceIndexPath];
-  BOOL shouldMove = YES;
-  if ([self.delegate respondsToSelector:@selector(tableViewModel:shouldMoveObject:atIndexPath:toIndexPath:inTableView:)]) {
-    shouldMove = [self.delegate tableViewModel:self shouldMoveObject:object atIndexPath:sourceIndexPath toIndexPath:destinationIndexPath inTableView:tableView];
-  }
-  if (shouldMove) {
-    [self removeObjectAtIndexPath:sourceIndexPath];
-    [self insertObject:object atRow:destinationIndexPath.row inSection:destinationIndexPath.section];
-  }
+    id object = [self objectAtIndexPath:sourceIndexPath];
+    BOOL shouldMove = YES;
+    if ([self.delegate respondsToSelector:@selector(tableViewModel:shouldMoveObject:atIndexPath:toIndexPath:inTableView:)]) {
+        shouldMove = [self.delegate tableViewModel:self shouldMoveObject:object atIndexPath:sourceIndexPath toIndexPath:destinationIndexPath inTableView:tableView];
+    }
+    if (shouldMove) {
+        [self removeObjectAtIndexPath:sourceIndexPath];
+        [self insertObject:object atRow:destinationIndexPath.row inSection:destinationIndexPath.section];
+    }
 }
 
 @end
@@ -221,10 +221,10 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NSMutableArray *)mutableRows {
-  NIDASSERT([self.rows isKindOfClass:[NSMutableArray class]] || nil == self.rows);
+    NIDASSERT([self.rows isKindOfClass:[NSMutableArray class]] || nil == self.rows);
     
-  self.rows = nil == self.rows ? [NSMutableArray array] : self.rows;
-  return (NSMutableArray *)self.rows;
+    self.rows = nil == self.rows ? [NSMutableArray array] : self.rows;
+    return (NSMutableArray *)self.rows;
 }
 
 @end
